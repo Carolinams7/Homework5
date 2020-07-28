7a402733841e32f176cc31a77f7d85b9701d5466
Carolina Szyman <szymancarolina@gmail.com>
2020-07-27 23:23:24 -0500

    Update Workscheduler.js

--- Workscheduler.js
+++ Workscheduler.js
@@ -5,6 +5,7 @@ var newDay = moment().hour(0);
 var currentTime = m.hour();
 
 // adding clock to currentDay id
+
 function clock() {
   var dateString = moment().format('MMMM Do YYYY, h:mm:ss a');
   $('#currentDay').html(dateString);
